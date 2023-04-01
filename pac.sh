2d143dd8d400b55c44ba32d62034e4fe89373453
d3ltaaa <109532675+d3ltaaa@users.noreply.github.com>
2023-04-01 23:30:44 +0200

    Update pac.sh

--- pac.sh
+++ pac.sh
@@ -24,6 +24,35 @@ yay -S picom-jonaburg-fix
 yay -S font-manager-git
 
 # set up nitrogen
+mkdir /home/falk/Pictures/Wallpapers
+
+
+# set up synergy
+# flatpak install synergy_1.14.6-snapshot.88fdd263.flatpak
+
+# set up remnote
+mkdir -p ~/Downloads
+cd ~/Downloads
+curl -L -o Remnote.AppImage https://www.remnote.com/desktop/linux/latest
+chmod +x Remnote.AppImage
+mkdir -p /opt/Remnote
+sudo mv Remnote.AppImage /opt/Remnote
+# Create a desktop file for the Remnote app
+sudo tee /usr/share/applications/remnote.desktop > /dev/null <<EOF
+[Desktop Entry]
+Name=Remnote
+Comment=Note-taking and spaced repetition software
+Exec=/opt/Remnote.AppImage
+Icon=remnote
+Terminal=false
+Type=Application
+Categories=Utility;TextEditor;
+EOF
+# Make the desktop file executable
+sudo chmod +x /usr/share/applications/remnote.desktop
+# Update your application list
+sudo update-desktop-database
+
 # set up awesome
 # set up picom
 # set up polybar
