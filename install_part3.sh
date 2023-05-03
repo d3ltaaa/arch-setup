7bd2fcf5f7e6a0242e9689aa0c39ddfcbd08ec7d
d3ltaaa <109532675+d3ltaaa@users.noreply.github.com>
2023-05-03 22:44:50 +0200

    Update install_part3.sh

--- install_part3.sh
+++ install_part3.sh
@@ -8,7 +8,7 @@ sudo pacman --noconfirm -S xf86-video-amdgpu xorg xorg-xinit webkit2gtk base-dev
 	bluez bluez-utils pulseaudio-bluetooth blueman \
 	firefox thunar \
 	nitrogen xournalpp discord \
-	neofetch ranger git neovim dunst\
+	neofetch ranger git neovim dunst xwallpaper \
 	flatpak unzip \
 	fuse2 ripgrep pamixer sox\
 	imagemagick
